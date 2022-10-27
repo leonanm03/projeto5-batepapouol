06a27e60c6530cb0f52b8e6f450c14e8a2e18598
leonanm03 <leonanm04@gmail.com>
2022-10-27 10:00:01 -0300

    mantendo conexão

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,45 @@
 
 
-const apilink = "https://mock-api.driven.com.br/api/v6/uol/participants"
-// const user = {name:prompt("Digite seu nome de usuário")}
+const apilink = "https://mock-api.driven.com.br/api/v6/uol"
+// let user = {name:prompt("Digite seu nome de usuário")}
+let connectionKeeper;
 
 let user = { name: "Leonan" };
 
-
-
-const promise = axios.post(apilink, user);
-promise.then(userRegistered);
-promise.catch(registerError);
+const globalrpomise = axios.post(apilink + "/participants", user);
+globalrpomise.then(userRegistered);
+globalrpomise.catch(registerError);
 
 function userRegistered(sucess) {
     alert("deu certo");
     console.log(sucess);
+    connectionKeeper = setInterval(keepConnection, 5000);
 }
 
 function registerError(error) {
     console.log(error.response.status);
     if (error.response.status === 400) {
         user = { name: prompt("Este nome já existe, tente outro") }
-        const tryagain = axios.post(apilink, user);
-        tryagain.then(userRegistered);
-        tryagain.catch(registerError);
+        const promise = axios.post(apilink + "/participants", user);
+        promise.then(userRegistered);
+        promise.catch(registerError);
     }
     else {
         alert("erro desconhecido, tente novamente mais tarde");
     }
 }
+
+function keepConnection() {
+    const promise = axios.post(apilink + "/status", user);
+    promise.then(connectionKept);
+    promise.catch(connectionError);
+}
+
+function connectionKept(sucess) {
+    console.log(sucess.status);
+}
+function connectionError(error) {
+    console.log(error.response.status);
+    alert("conexão perdida, recarregue a página e cadastre novamente");
+}
+
