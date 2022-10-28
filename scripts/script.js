bbb35992386daeb815eb7f275ecf972717eaa76e
leonanm03 <leonanm04@gmail.com>
2022-10-28 14:43:27 -0300

    enviam msgs públicas para todos(sem o bonus)

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 let messages = [] // array que guarda as mensagens do servidor
 
 const apilink = "https://mock-api.driven.com.br/api/v6/uol"
-// let user = {name:prompt("Digite seu nome de usuário")}
+let user = { name: prompt("Digite seu nome de usuário") }
 let connectionKeeper; // variável que armazena o número set interval do keepConection
 let messagesUpdater; // variável para armazenar o número set interval a requisição contínua de mensagens
-let user = { name: "Leonan" };
+// let user = { name: "Leonan" }; //usuário de teste (comentar a linha 3 se for usar)
 
 //LOGIN
 //primeira requisição de login
@@ -13,11 +13,10 @@ globalrpomise.then(userRegistered);
 globalrpomise.catch(registerError);
 
 // login bem sucedido
-function userRegistered(sucess) {
-    console.log(sucess);
-    connectionKeeper = setInterval(keepConnection, 5000);
-    searchMessages();
-    messagesUpdater = setInterval(searchMessages, 3000);
+function userRegistered() {
+    connectionKeeper = setInterval(keepConnection, 5000); //manter conexão a cada 5 segundos
+    searchMessages(); //busca mensagens
+    messagesUpdater = setInterval(searchMessages, 3000); // atualizar o chat a cada 3 segundos
 }
 
 // erro no login
@@ -35,7 +34,6 @@ function registerError(error) {
 }
 //LOGIN
 
-
 // MANTENDO CONEXÃO
 function keepConnection() {
     const promise = axios.post(apilink + "/status", user);
@@ -49,7 +47,8 @@ function connectionKept(sucess) {
 //erro de conexão
 function connectionError(error) {
     console.log(error.response.status);
-    alert("conexão perdida, recarregue a página e cadastre novamente");
+    alert("conexão perdida, recarregando página");
+    setTimeout(window.location.reload, 100);
 }
 // MANTENDO CONEXÃO
 
@@ -80,11 +79,7 @@ function renderMessages() {
     for (let i = 0; i < messages.length; i++) {
         let message = messages[i];
         if (message.type === "status") {
-            ulMessages.innerHTML +=
-                `
-            <li class="access-alert">
-                        <p><span class="timestamp">(${message.time}) </span> <strong class="user-access">${message.from} </strong>${message.text}</p>
-                    </li>`
+            ulMessages.innerHTML += `<li class="access-alert"><p><span class="timestamp">(${message.time}) </span> <strong class="user-access">${message.from} </strong>${message.text}</p></li>`
 
         }
         else if (message.type === "message") {
@@ -92,12 +87,40 @@ function renderMessages() {
 
         }
         else if (message.type === "private_message") {
-            if (message.from === user || message.to === user) {
+            if (message.from === user.name || message.to === user.name) {
                 ulMessages.innerHTML += `<li class="msg-private"><p><span class="timestamp">(${message.time})</span> <strong class="sender">${message.from}</strong> para <strong class="receiver">${message.to}</strong>: ${message.text}</p></li>`
 
             }
         }
-        const scrollView = document.querySelectorAll("main>ul li");
-        scrollView[scrollView.length - 1].scrollIntoView();
     }
+    const scrollView = document.querySelectorAll("main>ul li");
+    scrollView[scrollView.length - 1].scrollIntoView();
+}
+
+function sendMessage() {
+
+    const msg = document.querySelector("footer>input");
+
+    const msg_object = {
+        from: user.name,
+        to: "Todos",
+        text: msg.value,
+        type: "message" // ou "private_message" para o bônus
+    }
+    msg.value = "";
+
+    const promise = axios.post(apilink + "/messages", msg_object);
+    promise.then(msgEnviada);
+    promise.catch(msgErro);
+
+}
+
+function msgEnviada() {
+    searchMessages();
+}
+
+function msgErro(error) {
+    alert("erro no envio! recarregando página");
+    setTimeout(window.location.reload, 100);
+    console.log(error);
 }
